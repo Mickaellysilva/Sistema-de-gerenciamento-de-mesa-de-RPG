88ae7581a323f1393b296a2842fe4ea0b53119ea
Mickaellysilva <mickaellycosta.dev@gmail.com>
2026-09-04 20:26:56 -0300

    Classe Mago criada

--- src/Mago.java
+++ src/Mago.java
@@ -1,2 +1,15 @@
-public class Mago {
+public class Mago extends Personagem{
+    private float inteligencia;
+
+    public Mago(int id, String nome, float nivel, float vida, float inteligencia){
+        super(id, nome, nivel, vida);
+        this.inteligencia = inteligencia;
+    }
+
+    @Override
+    public String atacar(){
+        return "O mago realiza o ataque com força"+ this.inteligencia+ "!";
+    }
 }
+
+
