3aca69a019c2faaf97702b872d59b077a1ed5401
Mickaellysilva <mickaellycosta.dev@gmail.com>
2026-09-04 20:08:26 -0300

    Classe guerreiro pronta

--- src/Guerreiro.java
+++ src/Guerreiro.java
@@ -1,2 +1,13 @@
-public class Guerreiro {
+public class Guerreiro extends Personagem {
+    private float forca;
+
+    public Guerreiro(int id, String nome, float nivel, float vida, float forca){
+        super(id, nome, nivel, vida);
+        this.forca = forca;
+    }
+
+    @Override
+    public String atacar() {
+        return "O Guerreiro realiza um ataque com força " + this.forca + "!";
+    }
 }
