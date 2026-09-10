f2437f11d4ac4187f46ea7000b06ff7aaea2ccec
Roberta Bezerra <104735377+roberta-bez@users.noreply.github.com>
2026-09-10 17:14:32 -0300

    Update Sessao.java
    Adicionando a código de sessão

--- src/Sessao.java
+++ src/Sessao.java
@@ -1,2 +1,39 @@
+import java.time.LocalDate;
+
 public class Sessao {
+
+    private int id;
+    private LocalDate data;
+    private String descricao;
+
+    public Sessao(){};
+
+    public Sessao(int id, LocalDate data, String descricao) {
+        this.id = id;
+        this.data = data;
+        this.descricao = descricao;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getData(){
+        return data;
+    }
+    public String getDescricao(){
+        return descricao;
+    }
+    public void setData(LocalDate data){
+        this.data=data;
+    }
+    public void setDescricao(String descricao){
+        this.descricao = descricao;
+    }
+
+    // atualização
 }
